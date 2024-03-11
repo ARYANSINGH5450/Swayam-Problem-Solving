cd9de1b73cc36ab7cab39e63582c141e331c978d
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-03-11 20:19:00 +0530

    Update and rename Tasks.txt to Counting 2.cpp

---
+++ Searching-1/Counting 2.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int bs(int ele,int a[],int n,int ans)
+{
+    int low=0,high=n-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(a[mid]>=ele)
+        {
+            ans=max(ans,n-mid);
+            high=mid-1;
+        }
+        else
+        {
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+int main()
+{
+  int n,q,arr[100],qarr[100];
+  cin>>n>>q;
+  for(int i=0;i<n;i++)
+    {
+      cin>>arr[i];
+    }
+   for(int i=0;i<q;i++)
+    {
+      cin>>qarr[i];
+    }
+  for(int i=0;i<q;i++)
+    {
+      cout<<bs(qarr[i],arr,n,0)<<" ";
+    }
+  return 0;
+}
