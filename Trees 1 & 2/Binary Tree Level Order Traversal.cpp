3dd8653a83d4247e0fb17b16a6aeb6a6267862f4
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-03-27 20:58:53 +0530

    Update Binary Tree Level Order Traversal.cpp

---
+++ Trees 1 & 2/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL) return ans;
+        queue<TreeNode *>q;
+        q.push(root);
+        int cnt=0;
+        TreeNode* curr;
+        vector<int>temp;
+        while(!q.empty())
+        {
+            cnt=q.size();
+            temp.clear();
+            for(int i=0;i<cnt;i++)
+            {
+                curr=q.front();
+                q.pop();
+                temp.push_back(curr->val);
+                if(curr->left!=NULL)
+                q.push(curr->left);
+                if(curr->right!=NULL)
+                q.push(curr->right);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
