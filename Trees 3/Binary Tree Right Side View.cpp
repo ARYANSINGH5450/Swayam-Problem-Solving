06f3a0697f4465310268c28102815c9f8baa3312
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-03-28 14:00:06 +0530

    Update Binary Tree Right Side View.cpp

--- Trees 3/Binary Tree Right Side View.cpp
+++ Trees 3/Binary Tree Right Side View.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        if(root==NULL)
+    void f(TreeNode *root,int l,vector<int>&res)
+    {
+       if (root==NULL)
+       {
+           return;
+       }
+        if (res.size()==l)
         {
-            return {};
+          res.push_back(root->val);
         }
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int>res;
-        while(!q.empty()) 
-        {
-            int count=q.size();
-            for(int i=count;i>0;i--) 
-            {
-                TreeNode* node=q.front();
-                q.pop(); 
-                if(i==count) 
-                {
-                    res.push_back(node->val);
-                }                
-                if(node->right!=NULL) 
-                {
-                    q.push(node->right);
-                }
-                if(node->left!=NULL) 
-                {
-                    q.push(node->left);
-                }
-            }
-        }
-        return res;
+        f(root->right,l+1,res);
+        f(root->left,l+1,res);
     }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>res;
+        f(root,0,res);
+        return res;
+     } 
 };
