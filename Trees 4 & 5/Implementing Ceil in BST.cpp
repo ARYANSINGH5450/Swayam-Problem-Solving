d2768cc48c4f1304bdc9b74c1e992a3789301c32
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-04-02 10:30:30 +0530

    Update Implementing Ceil in BST.cpp

---
+++ Trees 4 & 5/Implementing Ceil in BST.cpp
@@ -0,0 +1,19 @@
+int findCeil(Node* root, int input) {
+    int ans=-1;
+    while(root)
+    {
+    if(root->data==input)
+    {
+        return input;
+    }
+    if(root->data>input)
+    {
+        ans=root->data;
+        root=root->left;
+    }
+    else
+    {
+        root=root->right;
+    }
+    }
+return ans;
