0542885d3dad83a380ac0f032a274e7d8cdeda81
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-04-02 11:58:40 +0530

    Update Boundary Traversal of Binary Tree.cpp

--- Trees 4 & 5/Boundary Traversal of Binary Tree.cpp
+++ Trees 4 & 5/Boundary Traversal of Binary Tree.cpp
@@ -1,31 +1,101 @@
-class node{
-    public:
-    int ma,mi,s;
-    node(int mini,int maxi,int size)
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int val;
+    node* left;
+    node* right;
+    node(int data)
     {
-        this->mi=mini;
-        this->ma=maxi;
-        this->s=size;
+        val=data;
+        left=nullptr;
+        right=nullptr;
     }
 };
-class Solution{
-    public:
-    node lbst(Node* root)
+bool leaf(node* root)
+{
+    if(root->left==NULL&root->right==NULL)
     {
-        if(root==NULL)
-        {
-            return node(INT_MAX,INT_MIN,0);
-        }
-        node left=lbst(root->left);
-        node right=lbst(root->right);
-        if(left.ma<root->data&&root->data<right.mi)
-        {
-            return node(min(root->data,left.mi),max(root->data,right.ma),left.ma+right.ma+1);
-        }
-        return node(INT_MIN,INT_MAX,max(left.ma,right.ma));
+        return true;
     }
-    int largestBst(Node *root)
+    return false;
+}
+void leftb(node* root,vector<int>& res)
+{
+    node* temp=root->left;
+    while(temp)
     {
-    	return lbst(root).ma;
+        if(!leaf(temp))
+        {
+            res.push_back(temp->val);
+        }
+        if(temp->left)
+        {
+            temp=temp->left;
+        }
+        else
+        {
+            temp=temp->right;
+        }
     }
-};
+}
+void leafb(node* root,vector<int>& res)
+{
+    if(leaf(root))
+    {
+        res.push_back(root->val);
+        return;
+    }
+    if(root->left)
+    {
+        leafb(root->left,res);
+    }
+    if(root->right)
+    {
+        leafb(root->right,res);
+    }
+}
+void rightb(node* root,vector<int>& res)
+{
+    node* temp=root->right;
+    vector<int>v;
+    while(temp)
+    {
+        if(!leaf(temp))
+        {
+            v.push_back(temp->val);
+        }
+        if(temp->right)
+        {
+            temp=temp->right;
+        }
+        else
+        {
+            temp=temp->left;
+        }
+    }
+    for(int i=v.size()-1;i>=0;i--)
+    {
+        res.push_back(v[i]);
+    }
+}
+int main()
+{
+    vector<int>res;
+    node* root=new node(1);
+    root->left=new node(2);
+    root->right=new node(3);
+    root->left->left=new node(4);
+    root->left->right=new node(5);
+    if(!leaf(root))
+    {
+        res.push_back(root->val);
+    }
+    leftb(root,res);
+    leafb(root,res);
+    rightb(root,res);
+    for(auto i:res)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
