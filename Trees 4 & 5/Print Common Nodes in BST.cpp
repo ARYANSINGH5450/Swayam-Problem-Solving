ec4ba8a7d00e00fc5faec1114ff72859147050c9
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-04-02 13:21:16 +0530

    Update Print Common Nodes in BST.cpp

--- Trees 4 & 5/Print Common Nodes in BST.cpp
+++ Trees 4 & 5/Print Common Nodes in BST.cpp
@@ -30,18 +30,23 @@ void print(node* root)
     }
     if(s.count(root->val))
     {
-      cout<<root->val;
+      cout<<root->val<<" ";
     }
     print(root->left);
     print(root->right);
 }
 int main()
 {
-  node* root=new node(10);
-  root->left=new node(5);
-  root->right=new node(15);
-  root->left->left=new node(2);
-  root->left->right=new node(7);  
+  node* A=new node(10);
+  A->left=new node(5);
+  A->right=new node(15);
+  A->left->left=new node(2);
+  A->left->right=new node(7); 
+  node* B=new node(10);
+  B->left=new node(5);
+  B->right=new node(15);
+  B->left->left=new node(2);
+  B->left->right=new node(7); 
   f(A);
   print(B);
   return 0;
