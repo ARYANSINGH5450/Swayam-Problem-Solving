92b97d376d2796b9e2a1e52459bc529d903ca771
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-04-03 16:52:55 +0530

    Update Binary Tree to CDLL.cpp

---
+++ Trees 6/Binary Tree to CDLL.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+  public:
+    Node* add(Node* left,Node* right){
+    if(right==NULL)
+    {
+        return left;
+    }
+        
+    if(left==NULL)
+    {
+        return right;
+    }
+    Node* lt;
+    Node* rt;
+    lt=left->left;
+    rt=right->left;
+    lt->right=right;
+    right->left=lt;
+    left->left=rt;
+    rt->right=left;
+    return left;
+}
+Node *bTreeToCList(Node *root)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    Node* left;
+    Node* right;
+    left=bTreeToCList(root->left);
+    right=bTreeToCList(root->right);
+    root->left=root;
+    root->right=root;
+    return add(add(left,root),right);
+}
+};
