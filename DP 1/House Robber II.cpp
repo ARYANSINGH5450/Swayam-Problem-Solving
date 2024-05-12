391a908abf7ddff19b02bea14ad301d5e96fca48
ARYANSINGH5450 <112381795+ARYANSINGH5450@users.noreply.github.com>
2024-05-12 12:04:19 +0530

    Update House Robber II.cpp

--- DP 1/House Robber II.cpp
+++ DP 1/House Robber II.cpp
@@ -1,27 +1,5 @@
 class Solution {
 public:
-//Recursion For House Robber I
-    /*int f(int i,vector<int>&dp,int n,vector<int>&nums)
-    {
-        if(i==n-1)
-        {
-            return nums[i];
-        }
-        else if(i>=n)
-        {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int pick=nums[i]+f(i+2,dp,n,nums);
-        int npick=0+f(i+1,dp,n,nums);
-        return max(pick,npick);
-    }*/
-//Bottom-Up
-class Solution {
-public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)
